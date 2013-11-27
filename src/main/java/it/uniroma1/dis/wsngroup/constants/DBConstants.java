323f1d266473e8c2c828377b24465dafea99e319
Francesco Ficarola <ficarola@dis.uniroma1.it>
2013-11-27 16:15:46 -0500

    DIAG DB renamed.

--- src/main/java/it/uniroma1/dis/wsngroup/constants/DBConstants.java
+++ src/main/java/it/uniroma1/dis/wsngroup/constants/DBConstants.java
@@ -10,7 +10,7 @@ public class DBConstants {
 	public static final String DB_TYPE = "mysql";
 	public static final String DB_SERVER_HOSTNAME = "127.0.0.1";
 	public static final String DB_SERVER_PORT = "3306";
-	public static final String DIS_DB_NAME = "SocialDIS";
+	public static final String DIS_DB_NAME = "diagdb";
 	public static final String DIS_DB_USERNAME = "socialdis";
 	public static final String DIS_DB_PASSWORD = "s0c1ald1s";
 	public static final String DIS_TAG_TABLE_NAME = "TagsTable";
